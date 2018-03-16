90e0817b6db10e1c104cd4f20c5df9b15384017a
Karljohan Lundin Palmerius <karljohan.lundin.palmerius@liu.se>
2018-03-16 15:06:12 +0100

    Regression test of configuration of objects with inheritance

--- test/gmConfig/src/base_config_functionality.cpp
+++ test/gmConfig/src/base_config_functionality.cpp
@@ -18,20 +18,56 @@ OFI_PARAM(BaseOFI, Base, a, int, Base::setA);
 
 struct Sub : Base {
   int b;
+  std::shared_ptr<Sub> ptr;
   void setB(int v) { b = v; }
+  void setPtr(std::shared_ptr<Sub> p) { ptr = p; }
 };
 
 OFI_CREATE_SUB(SubOFI, Sub, &BaseOFI);
 OFI_PARAM(SubOFI, Sub, b, int, Sub::setB);
+OFI_POINTER(SubOFI, Sub, ptr, Sub, Sub::setPtr);
 
 std::string xml = ""
   "<config>"
   "  <Base a=\"412\"/>"
-  "  <Sub a=\"517\" b=\"917\"/>"
+  "  <Sub a=\"517\" b=\"917\">"
+  "    <Sub name=\"ptr\" a=\"1\" b=\"4\"/>"
+  "  </Sub>"
   "</config>";
 
-TEST(gmConfigBaseFunctionality, ) {
+TEST(gmConfigBaseFunctionality, Inheritance) {
   
   gmConfig::Configuration config(xml);
 
+  std::shared_ptr<Base> base;
+  EXPECT_TRUE(config.getObject(base));
+  EXPECT_TRUE(base);
+  EXPECT_EQ(412, base->a);
+
+  std::shared_ptr<Sub> sub;
+  EXPECT_TRUE(config.getObject(sub));
+  EXPECT_TRUE(sub);
+  if (sub) {
+    EXPECT_EQ(517, sub->a);
+    EXPECT_EQ(917, sub->b);
+    if (sub->ptr) {
+      EXPECT_TRUE(sub->ptr);
+      EXPECT_EQ(1, sub->ptr->a);
+      EXPECT_EQ(4, sub->ptr->b);
+    }
+  }
+}
+
+TEST(gmConfigBaseFunctionality, Tree) {
+  
+  gmConfig::Configuration config(xml);
+
+  std::shared_ptr<Sub> sub;
+  EXPECT_TRUE(config.getObject(sub));
+  EXPECT_TRUE(sub);
+  if (sub && sub->ptr) {
+    EXPECT_TRUE(sub->ptr);
+    EXPECT_EQ(1, sub->ptr->a);
+    EXPECT_EQ(4, sub->ptr->b);
+  }
 }
