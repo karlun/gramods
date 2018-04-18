8117791aac1a79844db0aa93b5b910f480d0da55
Karljohan Lundin Palmerius <karljohan.lundin.palmerius@liu.se>
2018-04-18 10:12:41 +0200

    Remove thread to simplify vrpn tracker - fetch upon getPose call

--- modules/gmTrack/include/gmTrack/VRPNTracker.hh
+++ modules/gmTrack/include/gmTrack/VRPNTracker.hh
@@ -7,9 +7,6 @@
 #ifdef GM_ENABLE_VRPN
 
 #include <vrpn_Tracker.h>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
 
 BEGIN_NAMESPACE_GMTRACK;
 
@@ -26,8 +23,6 @@ public:
 
   void setConnectionString(std::string id);
 
-  void setUpdateRate(double fps);
-
   /**
      Replaces the contents of p with pose data.
   */
@@ -37,13 +32,9 @@ private:
 
   std::unique_ptr<vrpn_Tracker_Remote> tracker;
 	static void VRPN_CALLBACK handler(void *userdata, const vrpn_TRACKERCB info);
-  void _update();
-  clock::duration frame_time;
 
-  std::thread thread;
-  std::mutex lock;
-  std::condition_variable cond;
   std::map<int, PoseSample> latest_samples;
+  bool got_data;
 };
 
 END_NAMESPACE_GMTRACK;
--- modules/gmTrack/src/VRPNTracker.cpp
+++ modules/gmTrack/src/VRPNTracker.cpp
@@ -1,60 +1,44 @@
 
 #include <gmTrack/VRPNTracker.hh>
 
-
 #ifdef GM_ENABLE_VRPN
+
+#include <gmCore/Console.hh>
 
 BEGIN_NAMESPACE_GMTRACK;
 
-VRPNTracker::VRPNTracker() {
-  setUpdateRate(30);
-}
+VRPNTracker::VRPNTracker() {}
 
 VRPNTracker::~VRPNTracker() {
   tracker = nullptr;
-  thread.join();
 }
 
 void VRPNTracker::setConnectionString(std::string id) {
-  std::lock_guard<std::mutex> guard(lock);
-
-  tracker = nullptr;
-  thread.join();
-
   tracker = std::make_unique<vrpn_Tracker_Remote>(id.c_str());
   tracker->register_change_handler(this, VRPNTracker::handler);
-  thread = std::thread(&VRPNTracker::_update, this);
-}
-
-void VRPNTracker::setUpdateRate(double fps) {
-  frame_time = std::chrono::duration_cast<clock::duration>
-    (std::chrono::duration<double>(1.0 / fps));
-  cond.notify_one();
 }
 
 bool VRPNTracker::getPose(std::map<int, PoseSample> &p) {
-  std::lock_guard<std::mutex> guard(lock);
-  p = latest_samples;
-}
 
-void VRPNTracker::_update() {
-
-  std::mutex mutex;
-  std::unique_lock<std::mutex> lock(mutex);
-
-  clock::time_point last_time = clock::now();
-  while (tracker) {
-
-    tracker->mainloop();
-
-    while (true) {
-      clock::time_point current_time = clock::now();
-      if (current_time >= last_time + frame_time)
-        break;
-      cond.wait_for(lock, frame_time - (current_time - last_time));
-    }
-    last_time = last_time + frame_time;
+  if (!tracker) {
+    GM_WRN("Tracker", "Cannot get pose - no vrpn connection");
+    return false;
   }
+
+  if (!tracker->connectionPtr()->doing_okay()) {
+    GM_WRN("Tracker", "Defunct connection - closing vrpn connection");
+    tracker = nullptr;
+    return false;
+  }
+
+  do {
+    got_data = false;
+    tracker->mainloop();
+  } while (got_data);
+
+  p = latest_samples;
+
+  return true;
 }
 
 void VRPN_CALLBACK VRPNTracker::handler(void *data, const vrpn_TRACKERCB info) {
@@ -64,17 +48,16 @@ void VRPN_CALLBACK VRPNTracker::handler(void *data, const vrpn_TRACKERCB info) {
     (std::chrono::seconds(info.msg_time.tv_sec));
   auto usecs = std::chrono::duration_cast<clock::duration>
     (std::chrono::microseconds(info.msg_time.tv_usec));
+  auto time = clock::time_point(secs + usecs);
 
-  {
-    std::lock_guard<std::mutex> guard(_this->lock);
+  _this->latest_samples[info.sensor].time = time;
+  _this->latest_samples[info.sensor].position =
+    Eigen::Vector3f(info.pos[0], info.pos[1], info.pos[2]);
+  _this->latest_samples[info.sensor].orientation =
+    Eigen::Quaternionf(info.quat[0], info.quat[1], info.quat[2], info.quat[3]);
 
-    _this->latest_samples[info.sensor].time =
-      clock::time_point(secs + usecs);
-    _this->latest_samples[info.sensor].position =
-      Eigen::Vector3f(info.pos[0], info.pos[1], info.pos[2]);
-    _this->latest_samples[info.sensor].orientation =
-      Eigen::Quaternionf(info.quat[0], info.quat[1], info.quat[2], info.quat[3]);
-  }
+  _this->got_data = true;
+  GM_VINF("Tracker", "Got vrpn tracker data for sensor " << info.sensor);
 }
 
 END_NAMESPACE_GMTRACK;
