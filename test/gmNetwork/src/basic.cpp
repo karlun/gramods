a7eb10bd9c0d52418c9a2eee6947b5e825893e4f
Karljohan Lundin Palmerius <karljohan.lundin.palmerius@liu.se>
2018-06-12 08:15:17 +0200

    Stop test program after 20 ms even with no connection

--- test/gmNetwork/src/basic.cpp
+++ test/gmNetwork/src/basic.cpp
@@ -8,50 +8,71 @@
 
 using namespace gramods;
 
-void NetSyncTest(std::string host, int delay_ms, int &count, bool &alive) {
-  std::stringstream ss_xml;
-  ss_xml << "<config>" << std::endl;
-  ss_xml << "  <NetSync>" << std::endl;
-  ss_xml << "    <param name=\"bind\" value=\"" << host << "\"/>" << std::endl;
-  ss_xml << "    <param name=\"peer\" value=\"127.0.0.1:20402\"/>" << std::endl;
-  ss_xml << "    <param name=\"peer\" value=\"127.0.0.1:20403\"/>" << std::endl;
-  //ss_xml << "    <param name=\"peer\" value=\"127.0.0.1:20404\"/>" << std::endl;
-  ss_xml << "  </NetSync>" << std::endl;
-  ss_xml << "</config>" << std::endl;
+struct Peer {
 
-  gmCore::Configuration config(ss_xml.str());
+  Peer(std::string host, int delay_ms, int &count)
+    : delay_ms(delay_ms),
+      count(count) {
+
+    std::stringstream ss_xml;
+    ss_xml << "<config>" << std::endl;
+    ss_xml << "  <NetSync>" << std::endl;
+    ss_xml << "    <param name=\"bind\" value=\"" << host << "\"/>" << std::endl;
+    ss_xml << "    <param name=\"peer\" value=\"127.0.0.1:20402\"/>" << std::endl;
+    ss_xml << "    <param name=\"peer\" value=\"127.0.0.1:20403\"/>" << std::endl;
+    //ss_xml << "    <param name=\"peer\" value=\"127.0.0.1:20404\"/>" << std::endl;
+    ss_xml << "  </NetSync>" << std::endl;
+    ss_xml << "</config>" << std::endl;
+
+    gmCore::Configuration config(ss_xml.str());
+
+    EXPECT_TRUE(config.getObject(netsync));
+
+    if (netsync)
+      thread = std::make_unique<std::thread>([this](){ this->run(); });
+  }
+
+  ~Peer() {
+    netsync = 0;
+    thread->join();
+  }
 
   std::shared_ptr<gmNetwork::NetSync> netsync;
-  EXPECT_TRUE(config.getObject(netsync));
+  std::unique_ptr<std::thread> thread;
+  int delay_ms;
+  int &count;
 
-  netsync->waitForConnection();
+  void run() {
+    netsync->waitForConnection();
 
-  while (alive) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(delay_ms));
-    ++count;
-    netsync->waitForAll();
+    while (netsync) {
+      ++count;
+      std::this_thread::sleep_for(std::chrono::milliseconds(delay_ms));
+      netsync->waitForAll();
+    }
   }
-}
+};
 
 TEST(gmNetworkNetSync, waitForAll) {
   int count0 = 0;
   int count1 = 0;
   int count2 = 0;
-  bool alive = true;
 
-  std::thread t0(NetSyncTest, std::string("127.0.0.1:20402"),
-                 1, std::ref(count0), std::ref(alive));
-  std::thread t1(NetSyncTest, std::string("127.0.0.1:20403"),
-                 2, std::ref(count1), std::ref(alive));
-  /*std::thread t2(NetSyncTest, std::string("127.0.0.1:20404"),
-                   5, std::ref(count2), std::ref(alive));*/
+  {
+    std::vector<std::shared_ptr<Peer>> peers;
+    peers.push_back(std::make_shared<Peer>(std::string("127.0.0.1:20402"),
+                                           1, std::ref(count0)));
+    peers.push_back(std::make_shared<Peer>(std::string("127.0.0.1:20403"),
+                                           2, std::ref(count1)));
+    /*peers.push_back(std::make_shared<Peer>(std::string("127.0.0.1:20404"),
+      5, std::ref(count1), std::ref(alive)));*/
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(20));
-  alive = false;
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+  }
 
-  t0.join();
-  t1.join();
-  //t2.join();
+  EXPECT_GT(count0, 0);
+  EXPECT_GT(count1, 0);
+  EXPECT_GT(count2, 0);
 
   EXPECT_EQ(count0, count1);
   EXPECT_EQ(count1, count2);
