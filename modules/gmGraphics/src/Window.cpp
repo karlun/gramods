cad4d92a813e7f982213af969abb3d06897c048a
Karljohan Lundin Palmerius <karljohan.lundin.palmerius@liu.se>
2018-09-09 12:37:18 +0200

    Correct type of window title is std::string

--- modules/gmGraphics/src/Window.cpp
+++ modules/gmGraphics/src/Window.cpp
@@ -5,6 +5,6 @@ BEGIN_NAMESPACE_GMGRAPHICS;
 
 GM_OFI_DEFINE(Window);
 GM_OFI_PARAM(Window, fullscreen, bool, Window::setFullscreen);
-GM_OFI_PARAM(Window, title, bool, Window::setTitle);
+GM_OFI_PARAM(Window, title, std::string, Window::setTitle);
 
 END_NAMESPACE_GMGRAPHICS;
