3858728f4dd694cad3409b62579f9db71b5a15fc
Karljohan Lundin Palmerius <karljohan.lundin.palmerius@liu.se>
2018-09-21 15:16:37 +0200

    Simple configuration loader

---
+++ apps/gm-load/src/main.cpp
@@ -0,0 +1,82 @@
+
+
+#include <gmCore/Configuration.hh>
+#include <gmCore/CommandLineParser.hh>
+#include <gmCore/OStreamMessageSink.hh>
+
+#include <gmGraphics/Window.hh>
+#include <gmGraphics/CallbackRenderer.hh>
+
+#include <tclap/CmdLine.h>
+
+
+using namespace gramods;
+
+int main(int argc, char *argv[]) {
+
+  TCLAP::CmdLine cmd
+    ("Simple configuration loader. Any functionality is encoded directly in the loaded configurations.");
+
+  TCLAP::MultiArg<std::string> arg_config_dummy
+    ("", "config",
+     "Configuration file(s) load.",
+     true, "file");
+  TCLAP::MultiArg<std::string> arg_xml_dummy
+    ("", "xml",
+     "XML configuration(s) to load.",
+     true, "string");
+  cmd.xorAdd(arg_config_dummy, arg_xml_dummy);
+
+  TCLAP::MultiArg<std::string> arg_param_dummy
+    ("", "param",
+     "XML configuration(s) to load.",
+     false, "identifier=value");
+  cmd.add(arg_param_dummy);
+
+  try {
+    cmd.parse(argc, argv);
+  } catch (TCLAP::ArgException &e) {
+    std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
+    return -1;
+  }
+
+  std::unique_ptr<gmCore::Configuration> config;
+  try {
+    config = std::make_unique<gmCore::Configuration>(argc, argv);
+  }
+  catch (const std::exception &ex) {
+    std::cerr << "Configuration error: " << ex.what() << std::endl;
+  }
+  catch(...) {
+    std::cerr << "Unknown internal error while creating Configuration instance." << std::endl;
+  }
+
+  if (!config) {
+    std::cerr << std::endl
+              << "No valid configuration available." << std::endl
+              << std::endl;
+    return -1;
+  }
+
+  std::vector<std::shared_ptr<gmGraphics::Window>> windows;
+  config->getAllObjects(windows);
+
+  std::vector<std::shared_ptr<gmCore::Object>> objects;
+  config->getAllObjects(objects);
+
+  bool alive = true;
+  while (alive) {
+
+    alive = windows.empty();
+
+    for (auto window : windows) {
+      window->processEvents();
+      window->renderFullPipeline();
+      window->swap();
+
+      alive |= window->isOpen();
+    }
+  }
+
+  return 0;
+}
