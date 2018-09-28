af82866a17ec29a7e292b556ad541279fb104f81
Karljohan Lundin Palmerius <karljohan.lundin.palmerius@liu.se>
2018-09-28 09:46:17 +0200

    Reading image texture with sequence support

--- modules/gmGraphics/include/gmGraphics/ImageTexture.hh
+++ modules/gmGraphics/include/gmGraphics/ImageTexture.hh
@@ -6,6 +6,7 @@
 
 #ifdef gramods_ENABLE_FreeImage
 
+#include <gmTypes/all.hh>
 #include <gmCore/OFactory.hh>
 #include <gmGraphics/Texture.hh>
 #include <memory>
@@ -37,9 +38,25 @@ public:
   GLuint getGLTextureID();
 
   /**
-     Set the file to read image data from.
-   */
-  void setFile(std::string file) { this->file = file; }
+     Set the file to read image data from. If a sequence of image
+     files should be read, set this as a fprint formatted template and
+     use setRange to specify the range. For example if file is set to
+     "image_%05d.png" and range is set to 1-199, then images from
+     "image_00000.png" to "image_00199.png" will be read.
+  */
+  void setFile(std::string file);
+
+  /**
+     Sets the range (inclusive) of frames to read. This assumes that
+     the file name specified with setFile is a printf formatted
+     template.
+  */
+  void setRange(gmTypes::size2 range);
+
+  /**
+     Activates or deactivates looping the animation.
+  */
+  void setLoop(bool on);
 
   GM_OFI_DECLARE;
 
@@ -47,9 +64,6 @@ private:
 
   class _This;
   std::unique_ptr<_This> _this;
-
-  std::string file;
-
 };
 
 END_NAMESPACE_GMGRAPHICS;
--- modules/gmGraphics/src/ImageTexture.cpp
+++ modules/gmGraphics/src/ImageTexture.cpp
@@ -3,18 +3,55 @@
 
 #ifdef gramods_ENABLE_FreeImage
 
+#include <FreeImage.h>
+
 BEGIN_NAMESPACE_GMGRAPHICS;
 
+GM_OFI_DEFINE(ImageTexture);
+GM_OFI_PARAM(ImageTexture, file, std::string, ImageTexture::setFile);
+GM_OFI_PARAM(ImageTexture, range, gmTypes::size2, ImageTexture::setRange);
+GM_OFI_PARAM(ImageTexture, loop, bool, ImageTexture::setLoop);
+
+
 namespace {
-  struct _This {
-
-    _This();
-
-    GLuint getGLTextureID() { return texture_id; }
-
-    GLuint texture_id = 0;
-    std::shared_ptr<FreeImageInitializer> free_image_initializer;
+  struct FreeImage {
+    FreeImage();
+    ~FreeImage();
   };
+
+  void freeimage_output(FREE_IMAGE_FORMAT fif, const char *msg) {
+    GM_ERR("FreeImage", msg);
+  }
+}
+struct ImageTexture::_This {
+
+  _This();
+
+  GLuint getGLTextureID() { return texture_id; }
+
+  void update();
+  bool loadImage(std::string filename, size_t frame = 0);
+
+  GLuint texture_id = 0;
+  std::string file = "";
+  bool fail = false;
+  gmTypes::size2 animation_range;
+  size_t animation_frame = 0;
+  bool animate = false;
+  bool loop = false;
+
+  static FreeImage free_image;
+};
+
+FreeImage ImageTexture::_This::free_image;
+
+FreeImage::FreeImage() {
+  FreeImage_Initialise();
+  FreeImage_SetOutputMessage(freeimage_output);
+}
+
+FreeImage::~FreeImage() {
+  FreeImage_DeInitialise();
 }
 
 ImageTexture::ImageTexture()
@@ -24,54 +61,169 @@ void ImageTexture::initialize() {
   Texture::initialize();
 }
 
-_This::_This()
-  : free_image_initializer(FreeImageInitializer::get()) {}
+void ImageTexture::setFile(std::string file) {
+  _this->file = file;
+}
 
-bool _This::loadImage(std::string filename) {
+void ImageTexture::setRange(gmTypes::size2 range) {
+  _this->animation_range = range;
+  _this->animation_frame = range[0];
+  _this->animate = true;
+}
 
-	FREE_IMAGE_FORMAT format = FreeImage_GetFileType(filename, 0);
-	if(format == FIF_UNKNOWN) 
-		format = FreeImage_GetFIFFromFilename(filename);
-	if(format == FIF_UNKNOWN)
+void ImageTexture::setLoop(bool on) {
+  _this->loop = on;
+}
+
+ImageTexture::_This::_This() {}
+
+void ImageTexture::_This::update() {
+
+  if (fail) return;
+
+  if (animate) {
+    GM_VINF("ImageTexture", "Animation frame " << animation_frame);
+    fail = !loadImage(file, animation_frame);
+
+    if (++animation_frame > animation_range[1])
+      if (loop) {
+        GM_VINF("ImageTexture", "Looping animation");
+        animation_frame = animation_range[0];
+      } else {
+        GM_VINF("ImageTexture", "Animation done");
+        animate = false;
+      }
+
+  } else if (!texture_id) {
+    fail = !loadImage(file);
+  }
+}
+
+bool ImageTexture::_This::loadImage(std::string file_template, size_t frame) {
+
+  // Arbitrary padding for numbers
+  size_t filename_buffer_size = file_template.size() + 1024;
+  char filename[filename_buffer_size];
+  snprintf(filename, filename_buffer_size, file_template.c_str(), frame);
+
+	FREE_IMAGE_FORMAT image_format = FreeImage_GetFileType(filename, 0);
+	if(image_format == FIF_UNKNOWN)
+		image_format = FreeImage_GetFIFFromFilename(filename);
+	if(image_format == FIF_UNKNOWN) {
+    GM_ERR("ImageTexture", "Unknown image file type of file '" << filename << "'");
 		return false;
+  }
 
-	FIBITMAP *image = nullptr;
-	if(FreeImage_FIFSupportsReading(format))
-		image = FreeImage_Load(format, filename);
-	//if the image failed to load, return failure
-	if(!image)
+  if (!FreeImage_FIFSupportsReading(image_format)) {
+    GM_ERR("ImageTexture", "No read support for image '" << filename << "'");
 		return false;
+  }
+
+	FIBITMAP *image = FreeImage_Load(image_format, filename);
+	if(!image) {
+    GM_ERR("ImageTexture", "Could not load image '" << filename << "'");
+		return false;
+  }
+
+  FREE_IMAGE_TYPE image_type = FreeImage_GetImageType(image);
+  GLenum gl_format;
+  GLenum gl_type;
+  switch (image_type) {
+  case FIT_BITMAP:
+    switch (FreeImage_GetBPP(image)) {
+    case 24:
+      gl_format = GL_BGR;
+      gl_type = GL_UNSIGNED_BYTE;
+      break;
+    case 32:
+      gl_format = GL_BGRA;
+      gl_type = GL_UNSIGNED_BYTE;
+      break;
+    default:
+      GM_ERR("ImageTexture", "Unsupported image format (" << FreeImage_GetBPP(image) << " bits per pixel)");
+      FreeImage_Unload(image);
+      return false;
+    }
+    break;
+  case FIT_UINT16:
+    gl_format = GL_RED;
+    gl_type = GL_UNSIGNED_SHORT;
+    break;
+	case FIT_INT16:
+    gl_format = GL_RED;
+    gl_type = GL_SHORT;
+    break;
+	case FIT_UINT32:
+    gl_format = GL_RED;
+    gl_type =  GL_UNSIGNED_INT;
+    break;
+	case FIT_INT32:
+    gl_format = GL_RED;
+    gl_type = GL_INT;
+    break;
+	case FIT_FLOAT:
+    gl_format = GL_RED;
+    gl_type = GL_FLOAT;
+    break;
+	case FIT_RGB16:
+    gl_format = GL_RGB;
+    gl_type = GL_SHORT;
+    break;
+	case FIT_RGBA16:
+    gl_format = GL_RGBA;
+    gl_type = GL_SHORT;
+    break;
+	case FIT_RGBF:
+    gl_format = GL_RGB;
+    gl_type = GL_FLOAT;
+    break;
+	case FIT_RGBAF:
+    gl_format = GL_RGBA;
+    gl_type = GL_FLOAT;
+    break;
+  default:
+    GM_ERR("ImageTexture", "Unknown pixel type (" << image_type << ") of image '" << filename << "'");
+    FreeImage_Unload(image);
+    return false;
+  }
 
 	BYTE* image_data = FreeImage_GetBits(image);
 	unsigned int image_width = FreeImage_GetWidth(image);
 	unsigned int image_height = FreeImage_GetHeight(image);
-	if((image_data == 0) || (image_width == 0) || (image_height == 0))
+	if((image_data == 0) || (image_width == 0) || (image_height == 0)) {
+    FreeImage_Unload(image);
 		return false;
-	
-	//generate an OpenGL texture ID for this texture
-	glGenTextures(1, &gl_texID);
-	//store the texture ID mapping
-	m_texID[texID] = gl_texID;
-	//bind to the new texture ID
-	glBindTexture(GL_TEXTURE_2D, gl_texID);
-	//store the texture data for OpenGL use
-	glTexImage2D(GL_TEXTURE_2D, level, internal_format, width, height,
-		border, image_format, GL_UNSIGNED_BYTE, bits);
+  }
 
-	//Free FreeImage's copy of the data
+  if (!texture_id) {
+    glGenTextures(1, &texture_id);
+  }
+
+  glBindTexture(GL_TEXTURE_2D, texture_id);
+  glTexImage2D(GL_TEXTURE_2D,
+               0, GL_RGBA, image_width, image_height,
+               0, gl_format, gl_type, image_data);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glBindTexture(GL_TEXTURE_2D, 0);
+
 	FreeImage_Unload(image);
 
-	//return success
+  GM_VINF("ImageTexture", "Loaded"
+          << " image " << filename
+          << " " << image_width << "x" << image_height
+          );
 	return true;
 }
 
-void ImageTexture::update() {}
+void ImageTexture::update() {
+  _this->update();
+}
 
-GLuint getGLTextureID() {
+GLuint ImageTexture::getGLTextureID() {
   return _this->getGLTextureID();
 }
 
 END_NAMESPACE_GMGRAPHICS;
 
-#endif
 #endif
