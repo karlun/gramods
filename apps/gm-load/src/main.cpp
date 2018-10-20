a4c0c43dc5691e7985ea7e399006b0b1c3623c9a
Karljohan Lundin Palmerius <karljohan.lundin.palmerius@liu.se>
2018-10-20 08:48:05 +0200

    Print separate step timing information for debugging purposes

--- apps/gm-load/src/main.cpp
+++ apps/gm-load/src/main.cpp
@@ -10,6 +10,8 @@
 
 #include <tclap/CmdLine.h>
 
+#include <chrono>
+
 
 using namespace gramods;
 
@@ -65,23 +67,74 @@ int main(int argc, char *argv[]) {
   std::vector<std::shared_ptr<gmCore::Object>> objects;
   config->getAllObjects(objects);
 
+  typedef std::chrono::steady_clock clock;
+  typedef std::chrono::duration<double, std::ratio<1>> d_seconds;
+
+  auto last_print_time = std::chrono::steady_clock::now();
+  size_t frame_count = 0;
+
+  d_seconds event_time;
+  d_seconds render_time;
+  d_seconds swap_time;
+  d_seconds update_time;
+
   bool alive = true;
   while (alive) {
 
     alive = windows.empty();
 
+    auto t0 = std::chrono::steady_clock::now();
+
     for (auto window : windows) {
-
       window->processEvents();
+    }
+
+    auto t1 = std::chrono::steady_clock::now();
+
+    for (auto window : windows) {
       if (!window->isOpen()) continue;
-
       window->RendererDispatcher::renderFullPipeline();
-      window->swap();
+    }
 
+    auto t2 = std::chrono::steady_clock::now();
+
+    for (auto window : windows) {
+      if (!window->isOpen()) continue;
+      window->swap();
       alive |= true;
     }
 
+    auto t3 = std::chrono::steady_clock::now();
+
     gmCore::Updateable::updateAll();
+
+    auto t4 = std::chrono::steady_clock::now();
+
+    frame_count += 1;
+
+    event_time += std::chrono::duration_cast<d_seconds>(t1 - t0);
+    render_time += std::chrono::duration_cast<d_seconds>(t2 - t1);
+    swap_time += std::chrono::duration_cast<d_seconds>(t3 - t2);
+    update_time += std::chrono::duration_cast<d_seconds>(t4 - t3);
+
+    auto current_time = std::chrono::steady_clock::now();
+    auto dt = std::chrono::duration_cast<d_seconds>(current_time - last_print_time);
+    if (dt.count() > 2) {
+      float to_us = 1e6 / (float)frame_count;
+      GM_INF("gm-load", "Running at rate " << (frame_count / dt.count()) << " fps" << std::endl
+             << "events:    " << (int)(to_us * event_time.count()) << " \u00B5s" << std::endl
+             << "rendering: " << (int)(to_us * render_time.count()) << " \u00B5s" << std::endl
+             << "swapping:  " << (int)(to_us * swap_time.count()) << " \u00B5s" << std::endl
+             << "updating:  " << (int)(to_us * update_time.count()) << " \u00B5s");
+
+      last_print_time = current_time;
+      frame_count = 0;
+
+      event_time = d_seconds();
+      render_time = d_seconds();
+      swap_time = d_seconds();
+      update_time = d_seconds();
+    }
   }
 
   return 0;
