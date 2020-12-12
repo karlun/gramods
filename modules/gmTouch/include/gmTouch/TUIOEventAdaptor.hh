4ff0c107b462beab6c1d99c264e4254e99601a2d
Karljohan Lundin Palmerius <karljohan.lundin.palmerius@liu.se>
2020-12-12 21:24:00 +0100

    refact: Hide away TUIO adapter internals with pimpl

--- modules/gmTouch/include/gmTouch/TUIOEventAdaptor.hh
+++ modules/gmTouch/include/gmTouch/TUIOEventAdaptor.hh
@@ -8,9 +8,6 @@
 
 #include <gmTouch/TouchState.hh>
 
-#include <TuioListener.h>
-#include <TuioClient.h>
-
 #include <memory>
 #include <mutex>
 
@@ -32,10 +29,10 @@ BEGIN_NAMESPACE_GMTOUCH;
  * \endcode
  */
 class TUIOEventAdaptor
-  : public TouchState::EventAdaptor,
-    public TUIO::TuioListener {
+  : public TouchState::EventAdaptor {
 
 public:
+  TUIOEventAdaptor();
 
   /**
    * Connects the TUIO client with TCP to the specified address
@@ -61,49 +58,9 @@ public:
    */
   void done();
 
-  /**
-   * Called by Tuio to add a cursor object.
-   */
-  void addTuioCursor(TUIO::TuioCursor *tcur);
-
-  /**
-   * Called by Tuio to update a cursor object.
-   */
-  void updateTuioCursor(TUIO::TuioCursor *tcur);
-
-  /**
-   * Called by Tuio to remove a cursor object.
-   */
-  void removeTuioCursor(TUIO::TuioCursor *tcur);
-
-  void addTuioObject(TUIO::TuioObject *tobj) {}
-  void updateTuioObject(TUIO::TuioObject *tobj) {}
-  void removeTuioObject(TUIO::TuioObject *tobj) {}
-  void addTuioBlob(TUIO::TuioBlob *tblb) {}
-  void updateTuioBlob(TUIO::TuioBlob *tblb) {}
-  void removeTuioBlob(TUIO::TuioBlob *tblb) {}
-
-  void refresh(TUIO::TuioTime frameTime) {}
-
 private:
-
-  int width, height;
-  bool mouse_down = false;
-
-  std::unique_ptr<TUIO::OscReceiver> osc_receiver;
-  std::unique_ptr<TUIO::TuioClient> tuio_client;
-
-  std::mutex cursor_data_lock;
-
-  struct Cursor {
-    TouchState::TouchPointId id;
-    float x;
-    float y;
-    bool add;
-    double time;
-  };
-
-  std::vector<Cursor> cursor_data;
+  struct Impl;
+  std::unique_ptr<Impl> _impl;
 };
 
 END_NAMESPACE_GMTOUCH;
