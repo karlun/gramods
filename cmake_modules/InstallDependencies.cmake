329d16cefdb412943ee36b65694da75de01a2236
Karljohan Lundin Palmerius <karljohan.lundin.palmerius@liu.se>
2021-06-16 08:59:07 +0200

    fix: Third party dependency search and install only once for all

--- cmake_modules/InstallDependencies.cmake
+++ cmake_modules/InstallDependencies.cmake
@@ -1,13 +1,5 @@
 
-function(install_executable_dependencies EXEC_FILE)
-  IF (NOT gramods_INSTALL_DEPENDENCIES)
-    RETURN()
-  ENDIF()
-  IF (NOT EXEC_FILE)
-    MESSAGE(WARNING "install_executable_dependencies called without path to executable")
-    RETURN()
-  ENDIF()
-
+function(install_dependencies EXEC_FILES LIB_FILES)
   CMAKE_MINIMUM_REQUIRED(VERSION 3.14)
   CMAKE_POLICY(VERSION 3.14)
 
@@ -16,76 +8,32 @@ function(install_executable_dependencies EXEC_FILE)
   INSTALL(CODE "LIST(APPEND DEP_FOLDERS \"${CMAKE_PREFIX_PATH}\")")
   INSTALL(CODE "LIST(APPEND DEP_FOLDERS \"${CMAKE_LIBRARY_PATH}\")")
   INSTALL(CODE "LIST(APPEND DEP_FOLDERS \"${CMAKE_CURRENT_SOURCE_DIR}\")")
-  INSTALL(CODE "SET(EXEC_FILE \"${EXEC_FILE}\")")
+  INSTALL(CODE "SET(EXEC_FILES \"${EXEC_FILES}\")")
+  INSTALL(CODE "SET(LIB_FILES \"${LIB_FILES}\")")
 
   INSTALL(CODE [[
 
+    LIST(APPEND pre_exclude_regexes "api-ms-.*") # windows API
+    LIST(APPEND pre_exclude_regexes "ext-ms-.*") # windows API
+    LIST(APPEND post_exclude_regexes ".*WINDOWS[\\/]system32.*") # windows system dlls
+
     FILE(GET_RUNTIME_DEPENDENCIES
-      EXECUTABLES ${EXEC_FILE}
+      EXECUTABLES ${EXEC_FILES}
+      LIBRARIES ${LIB_FILES}
       RESOLVED_DEPENDENCIES_VAR _r_deps
       UNRESOLVED_DEPENDENCIES_VAR _u_deps
       CONFLICTING_DEPENDENCIES_PREFIX _c_deps
       DIRECTORIES ${DEP_FOLDERS}
-      PRE_EXCLUDE_REGEXES "api-ms-*"
-      POST_EXCLUDE_REGEXES ".*system32/.*\\.dll" ".*SysWOW64/.*\\.dll"
+      PRE_EXCLUDE_REGEXES ${pre_exclude_regexes}
+      POST_EXCLUDE_REGEXES ${post_exclude_regexes}
       )
 
     IF (_u_deps)
-      MESSAGE(WARNING "There were unresolved dependencies for executable ${EXEC_FILE}: \"${_u_deps}\"!")
+      MESSAGE(WARNING "There were unresolved dependencies: \"${_u_deps}\"!")
     ENDIF()
-    IF (_c_deps_FILENAMES)
-      MESSAGE(WARNING "There were conflicting dependencies for executable ${EXEC_FILE}: \"${_c_deps_FILENAMES}\"!")
-    ENDIF()
-
-    FOREACH(_file ${_r_deps})
-      FILE(INSTALL
-        DESTINATION "${CMAKE_INSTALL_PREFIX}/bin"
-        TYPE SHARED_LIBRARY
-        FOLLOW_SYMLINK_CHAIN
-        FILES "${_file}"
-        )
-    ENDFOREACH()
-    ]])
-
-endfunction()
-
-function(install_library_dependencies LIB_FILE)
-  IF (NOT gramods_INSTALL_DEPENDENCIES)
-    RETURN()
-  ENDIF()
-  IF (NOT LIB_FILE)
-    MESSAGE(WARNING "install_library_dependencies called without path to library")
-    RETURN()
-  ENDIF()
-
-  CMAKE_MINIMUM_REQUIRED(VERSION 3.14)
-  CMAKE_POLICY(VERSION 3.14)
-
-  # Path to search for third-party dependencies. This should include
-  # vcpkg if correctly configured
-  INSTALL(CODE "LIST(APPEND DEP_FOLDERS \"${CMAKE_PREFIX_PATH}\")")
-  INSTALL(CODE "LIST(APPEND DEP_FOLDERS \"${CMAKE_LIBRARY_PATH}\")")
-  INSTALL(CODE "LIST(APPEND DEP_FOLDERS \"${CMAKE_CURRENT_SOURCE_DIR}\")")
-  INSTALL(CODE "SET(LIB_FILE \"${LIB_FILE}\")")
-
-  INSTALL(CODE [[
-
-    FILE(GET_RUNTIME_DEPENDENCIES
-      LIBRARIES ${LIB_FILE}
-      RESOLVED_DEPENDENCIES_VAR _r_deps
-      UNRESOLVED_DEPENDENCIES_VAR _u_deps
-      CONFLICTING_DEPENDENCIES_PREFIX _c_deps
-      DIRECTORIES ${DEP_FOLDERS}
-      PRE_EXCLUDE_REGEXES "api-ms-*"
-      POST_EXCLUDE_REGEXES ".*system32/.*\\.dll"
-      )
-
-    IF (_u_deps)
-      MESSAGE(WARNING "There were unresolved dependencies for library ${LIB_FILE}: \"${_u_deps}\"!")
-    ENDIF()
-    IF (_c_deps_FILENAMES)
-      MESSAGE(WARNING "There were conflicting dependencies for library ${LIB_FILE}: \"${_c_deps}\"!")
-    ENDIF()
+    FOREACH (file ${_c_deps_FILENAMES})
+      MESSAGE(WARNING "There were conflicting dependencies for ${file}: ${_c_deps_${file}}!")
+    ENDFOREACH ()
 
     IF (WIN32)
       SET(LIB_FOLDER "bin")
