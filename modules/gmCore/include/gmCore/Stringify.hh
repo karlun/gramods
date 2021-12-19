8f8cb753de483984acbbcb4996ca9d1360bfa846
Karljohan Lundin Palmerius <karljohan.lundin.palmerius@liu.se>
2021-12-19 08:31:53 +0100

    feat: Functions to turn vectors and maps to strings

--- modules/gmCore/include/gmCore/Stringify.hh
+++ modules/gmCore/include/gmCore/Stringify.hh
@@ -3,7 +3,50 @@
 #define GRAMODS_CORE_STRINGIFY
 
 #include <sstream>
+#include <vector>
+#include <map>
 
 #define GM_STR(X) (static_cast<std::stringstream&>(std::stringstream().flush() << X).str())
+
+BEGIN_NAMESPACE_GMCORE;
+
+template<class T>
+std::string stringify(std::vector<T> list) {
+
+  std::stringstream ss;
+  bool is_first = true;
+
+  for (auto &item : list) {
+    if (!is_first) ss << ", ";
+    else is_first = false;
+    ss << item;
+  }
+
+  return ss.str();
+}
+
+template<class T1, class T2>
+std::string stringify(std::map<T1, T2> list, bool print_first = true) {
+
+  std::stringstream ss;
+  bool is_first = true;
+
+  if (print_first)
+    for (auto &item : list) {
+      if (!is_first) ss << ", ";
+      else is_first = false;
+      ss << item.first;
+    }
+  else
+    for (auto &item : list) {
+      if (!is_first) ss << ", ";
+      else is_first = false;
+      ss << item.second;
+    }
+
+  return ss.str();
+}
+
+END_NAMESPACE_GMCORE;
 
 #endif
