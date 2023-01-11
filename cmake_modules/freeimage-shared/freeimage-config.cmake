a57e99e3f23edeb7bc1c787278ee383a6326a2b5
Karljohan Lundin Palmerius <karljohan.lundin.palmerius@liu.se>
2023-01-11 16:19:22 +0100

    build: OpenCV and FreeImage use CMake config files

---
+++ cmake_modules/freeimage-shared/freeimage-config.cmake
@@ -0,0 +1,25 @@
+# Final resort if no "real" freeimage-config.cmake file is available
+
+if(NOT FreeImage_ROOT_DIR AND DEFINED ENV{FreeImage_ROOT_DIR})
+  SET(FreeImage_ROOT_DIR "$ENV{FreeImage_ROOT_DIR}" CACHE PATH
+    "FreeImage base directory location (optional, used for nonstandard installation paths)")
+endif()
+
+if(FreeImage_ROOT_DIR)
+  SET(FreeImage_INCLUDE_PATH PATHS "${FreeImage_ROOT_DIR}/include" NO_DEFAULT_PATH)
+  SET(FreeImage_LIBRARY_PATH PATHS "${FreeImage_ROOT_DIR}/lib"     NO_DEFAULT_PATH)
+endif()
+
+FIND_PATH(FreeImage_INCLUDE_DIR NAMES FreeImage.h PATH_SUFFIXES "FreeImage" ${FreeImage_INCLUDE_PATH})
+FIND_LIBRARY(FreeImage_LIBRARY NAMES freeimage PATH_SUFFIXES "FreeImage" ${FreeImage_LIBRARY_PATH})
+
+ADD_LIBRARY(FreeImage::FreeImage SHARED IMPORTED)
+IF(FreeImage_INCLUDE_DIR AND FreeImage_LIBRARY)
+  SET_TARGET_PROPERTIES(FreeImage::FreeImage PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES ${FreeImage_INCLUDE_DIR}
+    IMPORTED_LOCATION ${FreeImage_LIBRARY}
+    INTERFACE_LINK_LIBRARIES freeimage
+    )
+ELSE()
+  MESSAGE(SEND_ERROR "Could not set properties of target FreeImage::FreeImage - set FreeImage_INCLUDE_DIR and FreeImage_LIBRARY or unset FreeImage_DIR")
+ENDIF()
