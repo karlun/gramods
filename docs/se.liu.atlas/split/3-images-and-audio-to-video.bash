ef72c52e76724b8c3b2196e9b58038fb23423c66
Karljohan Lundin Palmerius <karljohan.lundin.palmerius@liu.se>
2023-12-15 13:48:52 +0100

    docs: Improved building splitted video files

--- docs/se.liu.atlas/split/3-images-and-audio-to-video.bash
+++ docs/se.liu.atlas/split/3-images-and-audio-to-video.bash
@@ -7,7 +7,8 @@ rm -rf cluster/node0 cluster/node1 cluster/node2 cluster/node3 cluster/node4 clu
 mkdir -p cluster/node0 cluster/node1 cluster/node2 cluster/node3 cluster/node4 cluster/node5 cluster/node6
 
 FFMPEG_SETTINGS="-c:v libx264 -r 30 -preset ultrafast -tune fastdecode -tune zerolatency"
-ffmpeg -i split/split0_%09d.png $FFMPEG_SETTINGS cluster/node0/film.mp4
+WAUDIO_SETTINGS="-c:a aac -shortest $FFMPEG_SETTINGS"
+ffmpeg -i split/split0_%09d.png -i audio.wav $WAUDIO_SETTINGS cluster/node0/film.mp4
 ffmpeg -i split/split1_%09d.png $FFMPEG_SETTINGS cluster/node1/film.mp4
 ffmpeg -i split/split2_%09d.png $FFMPEG_SETTINGS cluster/node2/film.mp4
 ffmpeg -i split/split3_%09d.png $FFMPEG_SETTINGS cluster/node3/film.mp4
