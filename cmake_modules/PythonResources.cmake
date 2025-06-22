0e42c3987f8473022cee65fb2be3c8b77198bb8c
Karljohan Lundin Palmerius <karljohan.lundin.palmerius@liu.se>
2025-06-22 21:36:37 +0200

    docs: Fixes to Python generated resources

--- cmake_modules/PythonResources.cmake
+++ cmake_modules/PythonResources.cmake
@@ -12,21 +12,25 @@ function (python_build_image script filename size)
     RETURN()
   ENDIF()
 
+  cmake_path(GET script PARENT_PATH SCRIPT_FOLDER)
+
   ADD_CUSTOM_COMMAND(
-    OUTPUT Pipfile.lock
+    OUTPUT ${SCRIPT_FOLDER}/Pipfile.lock
     COMMAND pwd
     COMMAND ${PipEnv} --bare install
-    COMMENT "Installing pipenv dependencies [${PipEnv} install]"
-    DEPENDS Pipfile
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    )
+    COMMENT "Installing pipenv dependencies in ${SCRIPT_FOLDER} [${PipEnv} install]"
+    DEPENDS ${SCRIPT_FOLDER}/Pipfile
+    WORKING_DIRECTORY ${SCRIPT_FOLDER}
+  )
+
+  cmake_path(GET script FILENAME SCRIPT_FILE)
 
   ADD_CUSTOM_COMMAND(
     OUTPUT ${filename}
-    DEPENDS ${script} Pipfile.lock
-    COMMAND ${PipEnv} run python ${script} -r ${size} -o ${filename} VERBATIM
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    )
+    DEPENDS ${script} ${SCRIPT_FOLDER}/Pipfile.lock
+    COMMAND ${PipEnv} run python ${SCRIPT_FILE} -r ${size} -o ${filename} VERBATIM
+    WORKING_DIRECTORY ${SCRIPT_FOLDER}
+  )
 
 endfunction()
 
