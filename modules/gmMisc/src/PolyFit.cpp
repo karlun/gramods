c1e4f182d5e0b2d13b09c40771f9df658e3c07ed
Karljohan Lundin Palmerius <karljohan.lundin.palmerius@liu.se>
2025-07-10 11:16:06 +0200

    feat: Estimating Jacobian matrix for Polynomial

--- modules/gmMisc/src/PolyFit.cpp
+++ modules/gmMisc/src/PolyFit.cpp
@@ -25,6 +25,8 @@ struct PolyFit::Impl {
   polco estimateCoefficients();
 
   std::vector<double> getValue(const std::vector<double> &inval) const;
+  Eigen::MatrixXd getJacobian(const std::vector<double> &in_values) const;
+  double getDerivative(double inval) const;
 
   void clear();
 
@@ -93,7 +95,7 @@ PolyFit::polco PolyFit::Impl::estimateCoefficients() {
 
   for (size_t sample_idx = 0; sample_idx < in_values.size(); ++sample_idx) {
 
-    // create list comp = [ [ 1, x, x^2 ], [ 1, y, y^2], ... ]
+    // Create list [ [ 1, x, x², ... ], [ 1, y, y², ...], ... ]
     std::vector<std::vector<double>> comp(IDIM);
     for (size_t idim = 0; idim < IDIM; ++idim) {
       std::vector<double> &vals = comp[idim];
@@ -108,7 +110,8 @@ PolyFit::polco PolyFit::Impl::estimateCoefficients() {
       }
     }
 
-    // create 1 * (1 + x + x^2 ) * (1 + y + y^2) ...
+    // Create list [ 1, x, x², ..., yx, yx², ..., y²x, y²x², ... ]
+    // by expanding [ 1, x, x², ... ] × [ 1, y, y², ... ] × ...
     std::vector<double> comp_val;
     comp_val.push_back(1.0);
     for (size_t idim = 0; idim < IDIM; ++idim) {
@@ -124,7 +127,7 @@ PolyFit::polco PolyFit::Impl::estimateCoefficients() {
       poly(sample_idx, comp_idx) = comp_val[comp_idx];
   }
 
-  auto XtX = poly.transpose() * poly; // NxM MxN -> NxN (order + 1)^2
+  auto XtX = poly.transpose() * poly; // NxM MxN -> NxN (order + 1)²
   auto XtXinv = XtX.inverse();
   auto XtXinvXt = XtXinv * poly.transpose();
 
@@ -153,6 +156,14 @@ PolyFit::getValue(const std::vector<double> &in_values) const {
   return _impl->getValue(in_values);
 }
 
+double PolyFit::getDerivative(double in_value) const {
+  if (_impl->IDIM != 1 || _impl->ODIM != 1)
+    throw gmCore::InvalidArgument(
+        GM_STR("Cannot use 1D method on polynomial with dimensions "
+               << _impl->IDIM << " and " << _impl->ODIM << "."));
+  return _impl->getDerivative(in_value);
+}
+
 std::vector<double>
 PolyFit::Impl::getValue(const std::vector<double> &in_values) const {
 
@@ -163,6 +174,7 @@ PolyFit::Impl::getValue(const std::vector<double> &in_values) const {
 
   const_cast<Impl *>(this)->estimateCoefficients();
 
+  // Create list [ [ 1, x, x², ... ], [ 1, y, y², ...], ... ]
   std::vector<std::vector<double>> comp(IDIM);
   for (size_t idim = 0; idim < IDIM; ++idim) {
     std::vector<double> &vals = comp[idim];
@@ -177,7 +189,8 @@ PolyFit::Impl::getValue(const std::vector<double> &in_values) const {
     }
   }
 
-  // create 1 * (1 + x + x^2 ) * (1 + y + y^2) ...
+  // Create list [ 1, x, x², ..., yx, yx², ..., y²x, y²x², ... ]
+  // by expanding [ 1, x, x², ... ] × [ 1, y, y², ... ] × ...
   std::vector<double> comp_val;
   comp_val.push_back(1.0);
   for (size_t idim = 0; idim < IDIM; ++idim) {
@@ -193,6 +206,86 @@ PolyFit::Impl::getValue(const std::vector<double> &in_values) const {
   for (size_t odim = 0; odim < ODIM; ++odim)
     for (size_t idx = 0; idx < COE; ++idx)
       res[odim] += comp_val[idx] * (*coeffs)(odim, idx);
+
+  return res;
+}
+
+Eigen::MatrixXd
+PolyFit::getJacobian(const std::vector<double> &in_values) const {
+  return _impl->getJacobian(in_values);
+}
+
+Eigen::MatrixXd
+PolyFit::Impl::getJacobian(const std::vector<double> &in_values) const {
+
+  if (in_values.size() != IDIM)
+    throw gmCore::InvalidArgument(GM_STR("Wrong size on polynomial input ("
+                                         << in_values.size() << " != " << IDIM
+                                         << ")."));
+
+  const_cast<Impl *>(this)->estimateCoefficients();
+
+  // Create list [ [ 1, x, x², ... ], [ 1, y, y², ...], ... ]
+  std::vector<std::vector<double>> comp(IDIM);
+  for (size_t idim = 0; idim < IDIM; ++idim) {
+    std::vector<double> &vals = comp[idim];
+    vals.reserve(ORD + 1);
+
+    double V = in_values[idim];
+    double T = 1;
+    vals.push_back(T);
+    for (size_t ord_idx = 0; ord_idx < ORD; ++ord_idx) {
+      T *= V;
+      vals.push_back(T);
+    }
+  }
+
+  Eigen::MatrixXd res(ODIM, IDIM);
+  for (size_t odim = 0; odim < ODIM; ++odim) {
+    for (size_t d_idim = 0; d_idim < IDIM; ++d_idim) {
+
+      // Expanding partial derivatives,
+      // e.g. [ 1, x, x², ... ] × [ 0, 1, y, ... ] × ...
+      std::vector<double> comp_val;
+      comp_val.push_back(1.0);
+      for (size_t idim = 0; idim < IDIM; ++idim) {
+        std::vector<double> res;
+        res.reserve(comp_val.size() * (ORD + 1));
+        for (size_t ord = 0; ord < ORD + 1; ++ord) {
+          const auto c = idim != d_idim ? comp[idim][ord]
+                         : ord > 0      ? ord * comp[idim][ord - 1]
+                                        : 0.0;
+          for (auto p : comp_val) res.push_back(c * p);
+        }
+        res.swap(comp_val);
+      }
+
+      for (size_t idx = 0; idx < COE; ++idx)
+        res(odim, d_idim) += comp_val[idx] * (*coeffs)(odim, idx);
+    }
+  }
+
+  return res;
+}
+
+double PolyFit::Impl::getDerivative(double in_value) const {
+
+  const_cast<Impl *>(this)->estimateCoefficients();
+
+  std::vector<double> comp;
+  comp.reserve(ORD);
+
+  double V = in_value;
+  double T = 1;
+  comp.push_back(T);
+  for (size_t idx = 0; idx < ORD - 1; ++idx) {
+    T *= V;
+    comp.push_back(T);
+  }
+
+  double res = 0.0;
+  for (size_t idx = 0; idx < COE - 1; ++idx)
+    res += (idx + 1) * comp[idx] * (*coeffs)(0, idx + 1);
 
   return res;
 }
