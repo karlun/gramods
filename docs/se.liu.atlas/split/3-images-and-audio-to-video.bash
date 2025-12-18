b96780efe15d569bbbead43fd1b522f2537e2ca2
Karljohan Lundin Palmerius <karljohan.lundin.palmerius@liu.se>
2025-12-18 13:30:08 +0100

    docs: Use H.265 to compress split movie

--- docs/se.liu.atlas/split/3-images-and-audio-to-video.bash
+++ docs/se.liu.atlas/split/3-images-and-audio-to-video.bash
@@ -6,7 +6,7 @@
 rm -rf cluster/node0 cluster/node1 cluster/node2 cluster/node3 cluster/node4 cluster/node5 cluster/node6
 mkdir -p cluster/node0 cluster/node1 cluster/node2 cluster/node3 cluster/node4 cluster/node5 cluster/node6
 
-FFMPEG_SETTINGS="-c:v libx264 -r 30 -preset ultrafast -tune fastdecode -tune zerolatency"
+FFMPEG_SETTINGS="-c:v libx265 -r 30 -preset ultrafast -tune fastdecode -tune zerolatency"
 WAUDIO_SETTINGS="-c:a aac -shortest $FFMPEG_SETTINGS"
 ffmpeg -i split/split0_%09d.png -i audio.wav $WAUDIO_SETTINGS cluster/node0/film.mp4
 ffmpeg -i split/split1_%09d.png $FFMPEG_SETTINGS cluster/node1/film.mp4
